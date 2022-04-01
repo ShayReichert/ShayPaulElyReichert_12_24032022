b8246dba9833fccb4ea8b6caa48178fa9514ce11
Shay Reichert <shay.reichert@outlook.fr>
2022-04-01 14:11:13 +0200

    Header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,28 @@
-import logo from "../../assets/logo.svg";
-import "./Header.scss";
+import logo from '../../assets/logo.svg'
+import './Header.scss'
 
 function Header() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <ul className="menu">
-        <li>Menu</li>
-        <li>Menu</li>
-        <li>Menu</li>
-        <li>Menu</li>
-      </ul>
+    <header className="header">
+      <div className="header-wrapper">
+        <img src={logo} className="logo" alt="logo" />
+        <ul className="menu">
+          <li>
+            <a href="#">Accueil</a>
+          </li>
+          <li>
+            <a href="#">Profil</a>
+          </li>
+          <li>
+            <a href="#">Réglage</a>
+          </li>
+          <li>
+            <a href="#">Communauté</a>
+          </li>
+        </ul>
+      </div>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
